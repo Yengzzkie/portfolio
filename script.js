07f17cb5dcf4b59330b1c4c22412a67b7ee456fc
Yengzzkie <gatchalian.manuel@gmail.com>
2024-01-27 11:19:16 -0500

    added social media icons, changed dark theme bgcolor to navy blue

--- script.js
+++ script.js
@@ -1,75 +1,56 @@
 // TYPING ANIMATION
 function runText() {
-    const text = "I am Manuel Gatchalian.";
-    const textContainer = document.querySelector(".content h1");
-    const delay = 100;
+  const text = "I am Manuel Gatchalian.";
+  const textContainer = document.querySelector(".content h1");
+  const delay = 100;
 
-    typeText(text, textContainer, delay);
+  typeText(text, textContainer, delay);
 }
 
 function typeText(text, textContainer, delay) {
-    for(let i = 0; i < text.length; i++) {
-        setTimeout(() => {
-            textContainer.textContent += text.charAt(i);
-        }, delay * i);
-    }
+  for (let i = 0; i < text.length; i++) {
+    setTimeout(() => {
+      textContainer.textContent += text.charAt(i);
+    }, delay * i);
+  }
 }
 
 window.addEventListener("DOMContentLoaded", () => {
-    setTimeout(() => {
-        runText();
-    }, 1000);
+  setTimeout(() => {
+    runText();
+  }, 1000);
 });
 
-
-
 // STARS GENERATOR
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener("DOMContentLoaded", function () {
+  const starsContainer = document.querySelector(".stars");
 
-    const starsContainer = document.querySelector('.stars');
-  
-    for (let i = 0; i < 100; i++) {
-      const star = document.createElement('div');
-      star.className = 'star';
-      star.style.left = Math.random() * 100 + 'vw';
-      star.style.top = Math.random() * 100 + 'vh';
-      starsContainer.appendChild(star);
-    }
-  });
+  for (let i = 0; i < 100; i++) {
+    const star = document.createElement("div");
+    star.className = "star-dot";
+    star.style.left = Math.random() * 100 + "vw";
+    star.style.top = Math.random() * 100 + "vh";
+    starsContainer.appendChild(star);
+  }
+});
 
 // MOON AND SUN TOGGLE
 
-const sunMoonBtn = document.querySelector(".toggle-theme")
+const sunMoonBtn = document.querySelector(".toggle-theme");
 
-sunMoonBtn.addEventListener('click', function() {
-    const moon = document.querySelector('.moon');
-    const hero = document.querySelector('.hero');
-    const about = document.querySelector('.about');
-    const contact = document.querySelector(".contact");
-    const projects = document.querySelector(".projects");
-    const blog = document.querySelector(".blog");
+sunMoonBtn.addEventListener("click", function () {
+  const moon = document.querySelector(".moon");
+  const hero = document.querySelector(".hero");
+  const about = document.querySelector(".about");
+  const contact = document.querySelector(".contact");
+  const projects = document.querySelector(".projects");
+  const blog = document.querySelector(".blog");
 
-    moon.classList.toggle('sun');
-    hero.classList.toggle("light");
-    about.classList.toggle("light");
-    contact.classList.toggle("light");
-    projects.classList.toggle("light");
-    blog.classList.toggle("light");
-})
-
-
-// PARALLAX EFFECT FOR MOON AND SUN
-// document.addEventListener("DOMContentLoaded", function () {
-//   const moon = document.querySelector(".moon");
-
-//   const parallaxSensitivity = 0.5;
-
-//   window.addEventListener("scroll", function () {
-//     const scrolledHeight = window.scrollY;
-//     const parallaxValue = scrolledHeight * parallaxSensitivity;
-
-//     moon.style.transform = `translateY(${parallaxValue}px)`;
-//   });
-// });
-
+  moon.classList.toggle("sun");
+  hero.classList.toggle("light");
+  about.classList.toggle("light");
+  contact.classList.toggle("light");
+  projects.classList.toggle("light");
+  blog.classList.toggle("light");
+});
 
